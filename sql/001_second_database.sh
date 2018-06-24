6f04ae19828aa1c6da8912f42ea234cefaac4ae8
Ken-M <ken@mori-mori.me>
2018-06-24 09:54:31 +0900

    fix sql initialize script.

--- sql/001_second_database.sh
+++ sql/001_second_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS \`integrated_measurement\` ;" | "${mysql[@]}"
-echo "GRANT ALL ON \`integrated_measurement\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
+echo "CREATE DATABASE IF NOT EXISTS \`integrated_measurement\` ;" | "$tempSqlFile"
+echo "GRANT ALL ON \`integrated_measurement\`.* TO '"$MYSQL_USER"'@'%' ;" | "$tempSqlFile"
+echo 'FLUSH PRIVILEGES ;' | "$tempSqlFile"
 
-"${mysql[@]}" < /docker-entrypoint-initdb.d/instantaneous_create_table.sql_
-"${mysql[@]}" < /docker-entrypoint-initdb.d/integrated_create_table.sql_
+"$tempSqlFile" < /docker-entrypoint-initdb.d/instantaneous_create_table.sql_
+"$tempSqlFile" < /docker-entrypoint-initdb.d/integrated_create_table.sql_
