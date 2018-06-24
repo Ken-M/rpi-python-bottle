b5071dec59f18afa7ad57af1af9c64b1aaf22088
Ken-M <ken@mori-mori.me>
2018-06-24 22:29:15 +0900

    sqlファイル修正

--- sql/integrated_create_table.sql
+++ sql/integrated_create_table.sql
@@ -1,17 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `integrated_measurement` ;
-GRANT ALL ON `integrated_measurement`.* TO '$MYSQL_USER'@'%' ;
+GRANT ALL ON `integrated_measurement`.* TO 'bottle'@'%' ;
 FLUSH PRIVILEGES ;
-
 USE integrated_measurement;
-
-CREATE TABLE IF NOT EXISTS `integrated_value` (
-  `server_id`    int(11) NOT NULL,
-  `integrated_power`  int(11) NOT NULL,
-  `power_delta`  int(11) NOT NULL,
-  `power_charge`  int(11) NOT NULL,
-  `created_at`   datetime NOT NULL PRIMARY KEY,
-  `created_user` int(11) NOT NULL,
-  `updated_at`   datetime NOT NULL,
-  `updated_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `integrated_value` (  `server_id`    int(11) NOT NULL,  `integrated_power`  int(11) NOT NULL, `power_delta`  int(11) NOT NULL,  `power_charge`  int(11) NOT NULL,  `created_at`   datetime NOT NULL PRIMARY KEY,  `created_user` int(11) NOT NULL,  `updated_at`   datetime NOT NULL,  `updated_user` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
