6640dad5b9041c2024ea7e5b61f44ffe23e822a2
Ken-M <ken@mori-mori.me>
2019-04-30 23:44:13 +0900

    add power type.

--- sql/integrated_create_table.sql
+++ sql/integrated_create_table.sql
@@ -2,5 +2,5 @@ CREATE DATABASE IF NOT EXISTS `integrated_measurement` ;
 GRANT ALL ON `integrated_measurement`.* TO 'bottle'@'%' ;
 FLUSH PRIVILEGES ;
 USE integrated_measurement;
-CREATE TABLE IF NOT EXISTS `integrated_value` (  `server_id`    int(11) NOT NULL,  `integrated_power`  double(20,3) NOT NULL, `power_delta`  double(20,3) NOT NULL,  `power_charge`  double(20,3) NOT NULL,  `created_at`   datetime NOT NULL PRIMARY KEY,  `created_user` int(11) NOT NULL,  `updated_at`   datetime NOT NULL,  `updated_user` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `integrated_value` (  `server_id`    int(11) NOT NULL,  `integrated_power`  double(20,3) NOT NULL, `power_delta`  double(20,3) NOT NULL,  `power_type` int(11) NOT NULL, `power_charge`  double(20,3) NOT NULL,  `created_at`   datetime NOT NULL PRIMARY KEY,  `created_user` int(11) NOT NULL,  `updated_at`   datetime NOT NULL,  `updated_user` int(11) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
